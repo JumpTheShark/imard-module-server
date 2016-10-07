4dd837a2680a02604b969421f87760b807224f7e
Oobluzhok Nachasic <nachasic@gmail.com>
2016-10-07 12:27:18 +0300

    Now importing all the elements from CDN

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ const appGenerator = function (importer) {
         res.locals.cache = false; // true in stage, production
         next();
     });
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
--- bin/bin.js
+++ bin/bin.js
@@ -1,53 +1,26 @@
 #! /usr/bin/env node
 
-// const nodemon = require('nodemon');
-//
-// nodemon({
-//     script: `${__dirname}/www.js`,
-//     watch: `${process.cwd()}/`
-// });
-//
-// console.log(process.cwd());
-// nodemon.on('start', () => {
-//     console.log("app has started");
-// });
-//
-// nodemon.on('change', (data) => {
-//     console.log(data);
-// });
-
 const forever = require('forever-monitor'),
-    watch = require('watch'),
     dirs = {
         script: `${__dirname}/www.js`,
         watch: `${process.cwd()}/`
-    };
+    },
+    server = new (forever.Monitor)(dirs.script, {
+        max: 1,
+        silent: true,
+        watch: true,               // Value indicating if we should watch files.
+        watchIgnoreDotFiles: true, // Whether to ignore file starting with a '.'
+        watchDirectory: dirs.watch,      // Top-level directory to watch from.
+        args: []
+    });
 
-let child = new (forever.Monitor)(dirs.script, {
-    max: 1,
-    silent: true,
-    watch: true,               // Value indicating if we should watch files.
-    watchIgnoreDotFiles: true, // Whether to ignore file starting with a '.'
-    watchDirectory: dirs.watch,      // Top-level directory to watch from.
-    args: []
-});
-
-child.on('exit', function (e) {
+server.on('exit', function (e) {
     console.log(e);
 });
 
-child.start();
+server.start();
 
-// watch.watchTree(dirs.watch, (f, curr, prev) => {
-//     if (typeof f == "object" && prev === null && curr === null) {
-//         // Finished walking the tree
-//     } else if (prev === null) {
-//         // f is a new file
-//     } else if (curr.nlink === 0) {
-//         // f was removed
-//     } else {
-//         console.log(`File changed:\n ${f}\nRestarting server`);
-//         child.restart();
-//     }
-// });
+
+
+
 
